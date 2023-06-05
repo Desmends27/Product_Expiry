ef53f29d1eae263a909dcd716c51f8390bd03edf
AndyGrld <101481355+AndyGrld@users.noreply.github.com>
2023-06-05 10:10:40 +0000

    Update setup.bat

--- setup/setup.bat
+++ setup/setup.bat
@@ -3,7 +3,7 @@
 python -m venv venv
 
 echo Activating virtual environment...
-venv\Scripts\activate.bat
+call venv\Scripts\activate.bat
 echo Installing requirements...
 pip install -r requirements.txt
 echo Done installing requirements

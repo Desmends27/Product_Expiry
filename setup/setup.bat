38fe0641423336a3f9997c0e6dc7ecd7d0a9cf91
AndyGrld <101481355+AndyGrld@users.noreply.github.com>
2023-06-05 10:03:43 +0000

    Update setup.bat

--- setup/setup.bat
+++ setup/setup.bat
@@ -1,14 +1,9 @@
 @echo off
 
-IF EXIST venv (
-    echo Virtual environment already exists
-) ELSE (
-    echo Creating virtual environment...
-    python -m venv venv
-)
+python -m venv venv
 
 echo Activating virtual environment...
-call venv\Scripts\activate.bat
+venv\Scripts\activate.bat
 echo Installing requirements...
 pip install -r requirements.txt
 echo Done installing requirements
@@ -18,8 +13,8 @@ python manage.py makemigrations
 python manage.py migrate
 
 echo "Creating .env file..."
-echo EMAIL_HOST_PASSWORD=Enter_password_here> .env
-echo EMAIL_HOST_USER=Enter_email_account_here>> .env
+echo EMAIL_HOST_PASSWORD=Enter_password_here > .env
+echo EMAIL_HOST_USER=Enter_email_account_here >> .env
 echo Done with setup...
 echo Please complete the .env file with required data...
 echo example EMAIL_HOST_USER=john@gmail.com
